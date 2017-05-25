f23624d9e78a5cf94866fa0fd712052a22a0947a
Kevin Blansit <kevin.blansit@gmail.com>
2017-05-24 23:51:34 -0700

    added caretakerintent

--- run-alexa-monitering.py
+++ run-alexa-monitering.py
@@ -8,14 +8,30 @@ from flask_ask import Ask, statement, question, session
 app = Flask(__name__)
 ask = Ask(app, '/')
 
+# define current user
+curr_user = None
+
+# define welcome message
 @ask.launch
 def welcome_msg():
+    # make welcome message
     speech_text = "Welcome to the discharge monitering application.\
     Is this Kevin or his caretaker?"
+
     return question(speech_text)
 
+# First level of question
+# define intents
 @ask.intent("CareTakerIntent")
-def
+def fever_question():
+    # set curr_user as caretaker
+    curr_user = "CARE_TAKER"
+
+    # confirmation that we're talking about the correct person
+    speech_text = "Alright, can we confirm the person we're monitering today?\
+    I current see that we're going over Kevin. Is this correct?"
+
+    return question(speech_text)
 
 if __name__ == '__main__':
     app.run()
