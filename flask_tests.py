d1c95e8eeea941597c9aae15186caeddc124aa68
Kevin Blansit <kevin.blansit@gmail.com>
2017-05-28 16:06:13 -0700

    reformatted yaml structure

--- flask_tests.py
+++ flask_tests.py
@@ -3,6 +3,7 @@
 # load libraries
 import yaml
 import unittest
+from itertools import product
 
 # load user defined libraries
 from src.Questionaire import QuestionNode
@@ -10,7 +11,7 @@ from src.Questionaire import QuestionNode
 class TestQuestionsStructure(unittest.TestCase):
 
     def setUp(self):
-        path = 'resources/application_text.yaml'
+        path = "resources/application_text.yaml"
         try:
             with open(path, "r") as f:
                 self.data = yaml.load(f)
@@ -18,9 +19,16 @@ class TestQuestionsStructure(unittest.TestCase):
             raise IOError("Cannot locate path: " + str(path))
 
     def test_question_loader(self):
-        import pdb; pdb.set_trace()
-        
-        pass
 
-if __name__ == '__main__':
+        # define basic queries
+        questions = self.data['application_settings']['master_questerion_ordering']
+        question_text = self.data['application_text']['question_text']
+        user = "care_taker"
+
+        # make linked list
+        prev_node = None
+        for i in questions[::-1]:
+            prev_node = QuestionNode(question_text[i][user], child_node=prev_node)
+
+if __name__ == "__main__":
     unittest.main()
--- src/Questionaire.py
+++ src/Questionaire.py
@@ -1,40 +1,21 @@
 #!/usr/bin/env python
 
 class QuestionNode:
-    def __init__(self):
-        # set defaults
-        self.children = []
-        self.question = None
+    def __init__(self, question=None, child_node=None):
+        # set either parameters or defaults
+        if question is None:
+            self.question = None
+        else:
+            self.question = question
 
-    # define setter functions
-    def add_child_response(self, child_node, response=None):
-        """
-        INPUTS:
-            child_node:
-                QuestionNode to be added as a child
-        EFFECT:
-            sets child_node as a child
-        """
+        if child_node is None:
+            self.child = None
+        else:
+            self.child = child_node
 
-        # add child to list
-        self.children.append()
-
-    def add_question(self, question):
-        """
-        INPUTS:
-            question:
-                the question to add (either string or SSML)
-        EFFECT:
-            adds child node as a child
-        """
-
-        # set question
-        self.question = question
-
-    def get_question(self):
-        """
-        RETURN:
-            the question (either string or SSML format)
-        """
-
-        return self.question
+    def __str__(self):
+        if type(self.question) == list:
+            for i in self.question:
+                return i + "\n"
+            else:
+                return self.question
