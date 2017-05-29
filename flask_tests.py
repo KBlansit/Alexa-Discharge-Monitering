f142d093ab6c361a48fc36cb1375b5d9e77e6f8f
Kevin Blansit <kevin.blansit@gmail.com>
2017-05-28 19:08:00 -0700

    added formal test for verifying that items are in master list

--- flask_tests.py
+++ flask_tests.py
@@ -2,6 +2,7 @@
 
 # load libraries
 import json
+import yaml
 import unittest
 import requests
 
@@ -10,16 +11,36 @@ from src.utilities import load_questions
 
 class TestQuestionsStructure(unittest.TestCase):
 
+    def test_question_list_valid(self):
+        """
+        Tests that list of questions is valid
+        """
+
+        # load data
+        path = "resources/application_settings.yaml"
+        try:
+            with open(path, "r") as f:
+                data = yaml.load(f)
+        except IOError:
+            raise IOError("Cannot locate path: " + str(path))
+
+        # define basic queries
+        questions = data['application_settings']['master_questerion_ordering']
+        question_text = data['application_text']['question_text']
+
+        for i in questions:
+            self.assertIn(i, question_text.keys())
+
     def test_question_loader(self):
         """
         Tests that both patient and care_taker can be used for all questions
         """
 
         # test
-        path = "resources/application_text.yaml"
+        path = "resources/application_settings.yaml"
         users = ["care_taker", "patient"]
         for i in users:
-            self.questions = load_questions(path, i)
+            load_questions(path, i)
 
 class TestFHIRQuestionnaireResponse(unittest.TestCase):
 
@@ -38,7 +59,6 @@ class TestFHIRQuestionnaireResponse(unittest.TestCase):
 
 
 
-        import pdb; pdb.set_trace()
 
         pass
 
