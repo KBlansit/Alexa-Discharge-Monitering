b95b184a65631d3b87c480a1c59acd5a70862017
Kevin Blansit <kevin.blansit@gmail.com>
2017-07-15 16:37:40 -0700

    added getter method for geting clinical questions and for listing clinical questions

--- src/Questionaire.py
+++ src/Questionaire.py
@@ -32,9 +32,11 @@ class Questionaire:
         INPUTS:
             question:
                 the question to ask
+        OUTPUT:
+            a question with the apprioprate text
         """
         # test if in dict
-        if question not in self.admin_questions.keys():
+        if question is not in self.admin_questions.keys():
             raise AssertionError(question + " is not a valid admin question")
 
         return question(self.admin_questions[question]['text'])
@@ -42,17 +44,44 @@ class Questionaire:
     def get_admin_response(statement):
         """
         INPUTS:
-            question:
-                the statement o respond with
+            statement:
+                the statement to respond with
+        OUTPUT:
+            a statement with the apprioprate text
         """
         # test if in dict
-        if statement not in self.admin_statements:
+        if statement is not in self.admin_statements:
             raise AssertionError(statement + " is not a valid admin statement")
 
         return statement(self.admin_statements[statement])
 
-    def get_clinical_question():
-        pass
+    def get_list_of_clinical_question(clinical_indication):
+        """
+        INPUTS:
+            clinical_indication:
+                the clinical indication to get questions for
+        OUTPUT:
+            a list of question keys for a given indication
+        """
+        # test if in dict
+        if clinical_indication is not in self.indication_questions.keys():
+            raise AssertionError(clinical_indication + " is not a valid clinical indication")
+
+        return self.indication_questions[clinical_indication]
+
+    def get_clinical_question(question):
+        """
+        INPUTS:
+            question:
+                the question to ask
+        OUTPUT:
+            a question with the apprioprate text
+        """
+        # test if in dict
+        if question is not in self.all_clinical_questions.keys():
+            raise AssertionError(question + " is not a valid clinical question")
+
+        return question(self.all_clinical_questions[question]['text'])
 
     def validate_answer(question, response_type):
         """
